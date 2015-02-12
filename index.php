334249f0b15c729b72747bbe5765a7efcd80973d
surajsingh51985 <suraj.singh@droom.in>
2015-02-12 16:26:10 +0530

    echo "test";

--- index.php
+++ index.php
@@ -1,13 +1,10 @@
 <?php
-<<<<<<< HEAD
+
 echo "This is testing file";
 
-?>
-=======
 
 phpinfo();
 echo "hello";
 echo "laravel";
 echo "welcome";
 ?>
->>>>>>> bd4cbdde13a96fd49a631e9c46918bf81e47e79c
